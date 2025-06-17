343a8a8f90769c39957b1528a48a1e5fd0112f5d
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-17 09:58:05 -0300

    Tipagem implícita ou por inferência

--- 02-tipagem-basica.ts
+++ 02-tipagem-basica.ts
@@ -14,3 +14,24 @@ console.log(`${nome} tem ${idade} anos.`);
 console.log(`Status: ${ativo ? "Ativo" : "Inativo"}`);
 
 separador();
+
+// Sobre o tipo any (qualquer)
+// Tipo neutro, usado SOMENTE em situações de exceção em que não é possivel rapidamente identificar qual o tipo do valor (exemplo: refatoração complexa de um projeto com JS antigo e que deve migrar pra TS)
+let qualquerCoisa: any = "Pode ser qualquer coisa";
+qualquerCoisa = 10;
+qualquerCoisa = true;
+
+console.log(`Qualquer coisa: ${qualquerCoisa}`);
+
+separador();
+
+// Sobre Tipagem implícita ou por inferência
+
+let exemplo1 = "Isso é uma string"; //TS infere que é string
+let exemplo2 = 10; //TS infere que é number
+
+//neste caso, não tem como TS inferir nada. Portanto, fica como any (EVITAR)
+let exemplo3;
+
+// variável sem valor mas já definida como tipo string
+let exemplo4: string;
