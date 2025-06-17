1a377961909e641d665b702adc59dac8073ef731
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-17 10:46:33 -0300

    funcoes com chamadas mais completas

--- 03-funcoes.ts
+++ 03-funcoes.ts
@@ -1,3 +1,5 @@
+import { separador } from "./modulos";
+
 export {};
 
 function saudacao(nome: string): string {
@@ -8,3 +10,22 @@ console.log(saudacao("ADA"));
 
 //console.log(saudacao(10)); //erro, é passado number
 //console.log(saudacao(true)); //erro, é passado boolean
+
+separador();
+
+//Uso de parâmetro opcional com a flag ? após o nome do parâmetro
+function saudacaoCompleta(
+  nome: string,
+  anoLetivo: number = 2025,
+  curso?: string
+) {
+  if (curso) {
+    return `Olá ${nome}, você esta estudando ${curso}, em ${anoLetivo}`;
+  }
+  return `Olá ${nome}, o cantor em ${anoLetivo}`;
+}
+
+//na chamada
+
+console.log(saudacaoCompleta("Valdemar", 2025, "historia"));
+console.log(saudacaoCompleta("Paulo", 2025));
--- dist/03-funcoes.js
+++ dist/03-funcoes.js
@@ -1,8 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const modulos_1 = require("./modulos");
 function saudacao(nome) {
     return `Olá ${nome}!`;
 }
 console.log(saudacao("ADA"));
-//console.log(saudacao(10)); //erro, é passado number
-//console.log(saudacao(true)); //erro, é passado boolean
+(0, modulos_1.separador)();
+function saudacaoCompleta(nome, anoLetivo = 2025, curso) {
+    if (curso) {
+        return `Olá ${nome}, você esta estudando ${curso}, em ${anoLetivo}`;
+    }
+    return `Olá ${nome}, o cantor em ${anoLetivo}`;
+}
+console.log(saudacaoCompleta("Valdemar", 2025, "historia"));
+console.log(saudacaoCompleta("Paulo", 2025));
