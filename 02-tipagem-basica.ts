fd83401afdcac5452dbd566568e63e471d90ab7f
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-17 09:38:22 -0300

    Tipos basicos
    number, string, booline

---
+++ 02-tipagem-basica.ts
@@ -0,0 +1,12 @@
+export {};
+
+//Exemplos de tipos básicos
+let idade: number = 30;
+let ativo: boolean = true;
+let nome: string = "Álvaro";
+
+//Nova atribuição
+idade = 31;
+
+console.log(`${nome} tem ${idade} anos.`);
+console.log(`Status: ${ativo ? "Ativo" : "Inativo"}`);
---
+++ dist/02-tipagem-basica.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Exemplos de tipos básicos
+let idade = 30;
+let ativo = true;
+let nome = "Álvaro";
+//Nova atribuição
+idade = 31;
+console.log(`${nome} tem ${idade} anos.`);
+console.log(`Status: ${ativo ? "Ativo" : "Inativo"}`);
