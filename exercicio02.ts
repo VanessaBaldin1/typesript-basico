6402a47a7d2a2ecb3a64cfe98e1a06818c55d2ba
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-18 11:56:12 -0300

    exercicio finalizar

--- exercicio02.ts
+++ exercicio02.ts
@@ -1,5 +1,33 @@
 //1) Crie uma interface para representar um produto com as propriedades nome, preço e garantia
 
-//2) Cria uma função que receba um objeto baseado na interface Produto e que mostre nome, preço e garantia
+interface Produto {
+  nome: string;
+  preco: number;
+  garantia: boolean;
+}
 
-//3)
+//2) Cria uma função que receba um objeto baseado na interface Produto e que mostre nome, preço e garantia. Obs: O objeto deve desestruturado como parâmetro individuais
+
+const exibirDados: Produto = ({
+  nome: string,
+  preco: number,
+  garantia,
+}: Produto) => {
+  console.log(`O ${nome}: ${preco} a garantia esta ${garantia}`);
+};
+
+const ProdutoA = {
+  nome: "Geladeira",
+  preco: 1500,
+  garantia: true,
+};
+
+const ProdutoB = {
+  nome: "TV",
+  preco: 500,
+  garantia: false,
+};
+
+exibirDados(ProdutoA);
+
+//3)Use a função pelo menos 2x passando produtos diferentes
