b426ac3191fdfdddf4e1b187939f7d577003c5f8
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-18 11:35:46 -0300

    exercicio de interface

---
+++ exercicio02.ts
@@ -0,0 +1,5 @@
+//1) Crie uma interface para representar um produto com as propriedades nome, preço e garantia
+
+//2) Cria uma função que receba um objeto baseado na interface Produto e que mostre nome, preço e garantia
+
+//3)
