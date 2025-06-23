e17beec7af311d3007d41db23bd16ccba0358a62
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-23 08:23:18 -0300

    resolução do exercicio

--- dist/exercicio02.js
+++ dist/exercicio02.js
@@ -1,1 +1,16 @@
 "use strict";
+const exibirDados = ({ nome, preco, garantia }) => {
+    console.log(`O ${nome}: ${preco} a garantia esta ${garantia}`);
+};
+const ProdutoA = {
+    nome: "Geladeira",
+    preco: 1500,
+    garantia: "2 amos",
+};
+const ProdutoB = {
+    nome: "TV",
+    preco: 500,
+    garantia: "1 ano",
+};
+exibirDados(ProdutoA);
+exibirDados(ProdutoB);
--- exercicio02.ts
+++ exercicio02.ts
@@ -3,31 +3,28 @@
 interface Produto {
   nome: string;
   preco: number;
-  garantia: boolean;
+  garantia: string;
 }
 
 //2) Cria uma função que receba um objeto baseado na interface Produto e que mostre nome, preço e garantia. Obs: O objeto deve desestruturado como parâmetro individuais
 
-const exibirDados: Produto = ({
-  nome: string,
-  preco: number,
-  garantia,
-}: Produto) => {
+const exibirDados = ({ nome, preco, garantia }: Produto) => {
   console.log(`O ${nome}: ${preco} a garantia esta ${garantia}`);
 };
 
 const ProdutoA = {
   nome: "Geladeira",
   preco: 1500,
-  garantia: true,
+  garantia: "2 amos",
 };
 
 const ProdutoB = {
   nome: "TV",
   preco: 500,
-  garantia: false,
+  garantia: "1 ano",
 };
 
 exibirDados(ProdutoA);
+exibirDados(ProdutoB);
 
 //3)Use a função pelo menos 2x passando produtos diferentes
